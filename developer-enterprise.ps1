702ea47bdf1873d616ff92513363669c5f6713b6
Kevin Seger <keteseg+github@gmail.com>
2018-06-25 11:21:50 +0200

    Fix for choco temp folder issue
    Added the creation of a folder to be used for the choco cache
    Added usage of the specific cache folder for each install

--- developer-enterprise.ps1
+++ developer-enterprise.ps1
@@ -1,3 +1,4 @@
+New-Item -Path $env:UserProfile\AppData\Local\ChocoCache -ItemType directory -force
 Disable-UAC
 
 #--- Initial Windows Config ---
@@ -19,57 +20,57 @@ cinst -y IIS-ManagementScriptingTools -source windowsfeatures
 cinst -y IIS-WindowsAuthentication -source windowsfeatures
 
 #--- .NET ---
-cinst PowerShell -y
-cinst dotnet4.5 -y
-cinst dotnet4.6.2 -y
-cinst dotnet4.7 -y
-cinst netfx-4.7.1-devpack -y
+cinst PowerShell -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnet4.5 -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnet4.6.2 -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnet4.7 -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst netfx-4.7.1-devpack -y -c "%UserProfile%\AppData\Local\ChocoCache"
 
-cinst dotnetcore-sdk -y
-cinst dotnetcore -y
-cinst dotnetcore-windowshosting -y
-cinst dotnetcore-runtime.install --version 1.0.10 -my
-cinst dotnetcore-runtime.install --version 1.1.7 -my
-cinst dotnetcore-runtime.install --version 2.0.7 -my
-cinst dotnetcore-runtime.install --version 2.1.0 -my
+cinst dotnetcore-sdk -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnetcore -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnetcore-windowshosting -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnetcore-runtime.install --version 1.0.10 -my -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnetcore-runtime.install --version 1.1.7 -my -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnetcore-runtime.install --version 2.0.7 -my -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst dotnetcore-runtime.install --version 2.1.0 -my -c "%UserProfile%\AppData\Local\ChocoCache"
 
 #--- Applications ---
-cinst googlechrome -y
-cinst javaruntime -y
-cinst notepadplusplus.install -y
-cinst notepad3.install -y
+cinst googlechrome -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst javaruntime -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst notepadplusplus.install -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst notepad3.install -y -c "%UserProfile%\AppData\Local\ChocoCache"
 
 #--- Visual Studio ---
-cinst visualstudio2015enterprise -y
+cinst visualstudio2015enterprise -y -c "%UserProfile%\AppData\Local\ChocoCache"
 
-cinst visualstudio2017enterprise -y
-cinst visualstudio2017-workload-manageddesktop -y
-cinst visualstudio2017-workload-netcoretools -y
-cinst visualstudio2017-workload-netweb -y
-cinst visualstudio2017-workload-node -y
+cinst visualstudio2017enterprise -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst visualstudio2017-workload-manageddesktop -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst visualstudio2017-workload-netcoretools -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst visualstudio2017-workload-netweb -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst visualstudio2017-workload-node -y -c "%UserProfile%\AppData\Local\ChocoCache"
 
 #--- Other dev ---
-cinst git -y
-cinst resharper-ultimate-all -y
-cinst sql-server-management-studio -y
-cinst poshgit -y
-cinst sourcetree -y
-cinst linqpad -y
-cinst github -y
-cinst docker-for-windows -y
-cinst docker-kitematic -y
-cinst redis-desktop-manager -y
-cinst rdcman -y
-cinst ProcExp -y
-cinst cloudberryexplorer.s3 -y
-cinst postman -y
-cinst sysinternals -y
-cinst python -y
-cinst awstools.powershell -y
-cinst pip -y
+cinst git -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst resharper-ultimate-all -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst sql-server-management-studio -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst poshgit -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst sourcetree -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst linqpad -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst github -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst docker-for-windows -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst docker-kitematic -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst redis-desktop-manager -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst rdcman -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst ProcExp -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst cloudberryexplorer.s3 -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst postman -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst sysinternals -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst python -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst awstools.powershell -y -c "%UserProfile%\AppData\Local\ChocoCache"
+cinst pip -y -c "%UserProfile%\AppData\Local\ChocoCache"
 
 #--- Visual Studio Code ---
-cinst visualstudiocode -y
+cinst visualstudiocode -y -c "%UserProfile%\AppData\Local\ChocoCache"
 
 code --install-extension ms-vscode.csharp
 code --install-extension ms-vscode.powershell
