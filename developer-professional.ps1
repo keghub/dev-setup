880b76b613972270e604d51f44edd42e29572af9
Kevin Seger <keteseg+github@gmail.com>
2019-08-20 16:50:55 +0200

    Changed Visual Studio Installations
     - Removed 2015
     - Re-added 2017
     - Added 2019

--- developer-professional.ps1
+++ developer-professional.ps1
@@ -43,14 +43,17 @@ cinst notepadplusplus.install -y --cacheLocation "$env:UserProfile\AppData\Local
 cinst notepad3.install -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
 
 #--- Visual Studio ---
-# Visual Studio installations currently does not work well, problems with hanging during installation
-# cinst visualstudio2015professional -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2017professional -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2017-workload-manageddesktop -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2017-workload-netcoretools -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2017-workload-netweb -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2017-workload-node -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
 
-# cinst visualstudio2017professional -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
-# cinst visualstudio2017-workload-manageddesktop -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
-# cinst visualstudio2017-workload-netcoretools -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
-# cinst visualstudio2017-workload-netweb -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
-# cinst visualstudio2017-workload-node -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2019professional -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2019-workload-manageddesktop -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2019-workload-netcoretools -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2019-workload-netweb -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
+cinst visualstudio2019-workload-node -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
 
 #--- Other dev ---
 cinst git -y --cacheLocation "$env:UserProfile\AppData\Local\ChocoCache"
